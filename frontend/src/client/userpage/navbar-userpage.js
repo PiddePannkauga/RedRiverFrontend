70380ab266ca8683a2d0907f7c3807463eb18093
nilslindkvist <lindkvist.nils@gmail.com>
2018-05-23 21:23:57 +0200

    Navbar minor fix Centered navlinks, right-align logout

--- frontend/src/client/userpage/navbar-userpage.js
+++ frontend/src/client/userpage/navbar-userpage.js
@@ -3,21 +3,30 @@ import Logo from '../../components/logo';
 
 
 const UserPageNavbar = (props) =>{
+  const buttonStyle = {
+    cursor: 'pointer'
+  };
+
   return(
-    <nav className="navbar navbar-expand navbar-light bg-light">
-
-      <Logo />
-
-      <div className="navbar" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a className="nav-link"  onClick={props.onClick} id="myEvents">Mina Event</a>
-          <a className="nav-link" onClick={props.onClick} id="applyEvent">Ansök till Event</a>
-          <a className="nav-link" onClick={props.onClick} id="profile">Profil</a>
-        </div>
-      </div>
-      <button type="button" className="btn btn-primary mb-2" onClick={props.logout}>Logga Ut</button>
+    <nav class="navbar navbar-light navbar-expand-md bg-faded justify-content-center">
+      <a href="/user" class="navbar-brand d-flex w-50 mr-auto"><Logo /></a>
+        <ul class="navbar-nav w-100 justify-content-center">
+          <li class="nav-item">
+              <a className="nav-link" style={buttonStyle} onClick={props.onClick} id="myEvents">Mina Event</a>
+          </li>
+          <li class="nav-item">
+              <a className="nav-link" style={buttonStyle} onClick={props.onClick} id="applyEvent">Ansök till Event</a>
+          </li>
+          <li class="nav-item">
+              <a className="nav-link" style={buttonStyle} onClick={props.onClick} id="profile">Profil</a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+          <li class="nav-item">
+              <button type="button" style={buttonStyle} className="btn btn-primary mb-2" onClick={props.logout}>Logga Ut</button>
+          </li>
+        </ul>
     </nav>
-
   )
 }
 
