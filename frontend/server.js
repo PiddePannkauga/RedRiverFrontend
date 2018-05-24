a0192a1f9fbda33ecbea145ad25c35220eb456bf
PiddePannkauga <petter.mansson1@gmail.com>
2018-05-24 21:43:38 +0200

    heroku build fix

--- frontend/server.js
+++ frontend/server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 4999;
 
-
+app.use(express.static(path.join(__dirname+'/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/build/index.html'));
 });
-app.use(express.static(path.join(__dirname+'/build')));
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
