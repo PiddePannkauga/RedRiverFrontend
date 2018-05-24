9456e55c1c068edb4ff18d7c954a0ef2a1963410
PiddePannkauga <petter.mansson1@gmail.com>
2018-05-24 18:37:14 +0200

    package.json in root. Changed start script

--- frontend/server.js
+++ frontend/server.js
@@ -3,14 +3,14 @@ const express = require('express');
 
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-// app.use(express.static(path.join(__dirname+'/build')));
+ app.use(express.static(path.join(__dirname+'/build')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
 
-
+console.log(path.join(__dirname+'/build/index.html', path.join(__dirname+'/build')));
 app.listen(port, () => console.log(`Listening on port ${port}`));
