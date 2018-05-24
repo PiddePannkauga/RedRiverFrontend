fd4b1b891b85756ef101d8541b3254d36056f1f6
PiddePannkauga <petter.mansson1@gmail.com>
2018-05-24 15:19:08 +0200

    Server change

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,14 +1,14 @@
 require('dotenv/config')
 const express = require('express');
 
-
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static("/home/petter/RedRiverFrontend/frontend/build"));
+app.use(express.static(path.join(__dirname+'/frontend/build')));
 
 app.get('/public', (req, res) => {
-  res.sendFile('/home/petter/RedRiverFrontend/frontend/build/index.html');
+  res.sendFile(path.join(__dirname+'+/frontend/build/index.html'));
 });
 
 
