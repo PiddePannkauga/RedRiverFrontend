9425411a36f0d95a1891717c2cd1d595a404b0d9
PiddePannkauga <petter.mansson1@gmail.com>
2018-05-24 20:32:35 +0200

    server commit

--- frontend/server.js
+++ frontend/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 4999;
 
 app.use(express.static(path.join(__dirname+'/build')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
